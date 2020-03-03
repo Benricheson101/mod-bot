f6f436bdd99ba43661f18f0ea9dfb487725a4580
Ben Richeson <Benricheson101@users.noreply.github.com>
2020-03-03 12:10:44 -0500

    Disable starboard event emitter for now

--- src/events/messageReactionAdd.ts
+++ src/events/messageReactionAdd.ts
@@ -6,7 +6,7 @@ import GuildDB = Database.GuildDB;
 export = async (client: Client, reaction: MessageReaction, user: User) => {
 	let guild: GuildDB = await reaction.message.guild.db;
 
-	if (reaction.emoji.name === "⭐" && guild.config.starboard.enabled) {
+	/* if (reaction.emoji.name === "⭐" && guild.config.starboard.enabled) {
 		client.emit("starboardAdd", reaction.message);
-	}
+	}*/
 }
